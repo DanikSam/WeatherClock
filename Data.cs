ce5d425d0a4643502c46d186a5f9718123a9e6d3
DaniilSamokaev <samokaev_00@mail.ru>
2019-06-12 12:53:10 +0300

    3 commit

--- Data.cs
+++ Data.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 
 namespace WeatherClock
 {
-    public class Class
+    public class ArrayOfData
     {
+        public int[] Hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+        public int[] Degree = [21, 24, 23, 20, 25, 26, 21, 23, 26, 30, 27, 24];
     }
 }
